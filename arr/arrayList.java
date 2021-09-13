f569286033cb3726228250587b71091809712b82
mubashir <mubashir11131719@gmail.com>
2021-09-13 20:27:57 +0530

    all subArrays

---
+++ arr/arrayList.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr =new ArrayList<Integer>(Arrays.asList("));
+
+        System.out.println(arr.size());
+    }
+}
